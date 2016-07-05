a36b30a19ebb352427c47472eb933fab26ad1db6
ayrtonteshima <ayrtonteshima@gmail.com>
2016-07-05 09:21:20 -0300

    Lembrete todo

--- app/handlers/search.js
+++ app/handlers/search.js
@@ -1,5 +1,5 @@
 import * as searchStore from './../stores/search';
-
+//TODO: Validar retorno com Joi
 export function filter(request, reply) {
     searchStore.filter(request).then(data => {
         reply({
