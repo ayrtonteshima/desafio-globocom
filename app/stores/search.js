b66e7cab2293317b83ee61e1524c18ebd3e51f4b
ayrtonteshima <ayrtonteshima@gmail.com>
2016-07-05 09:11:29 -0300

    Adicionando funcionalidades para filtrar os hightlights

--- app/stores/search.js
+++ app/stores/search.js
@@ -1,6 +1,34 @@
 import * as searchModel from './../models/search';
 
-const filterHightlights = (term, { hightlights }) => hightlights;
+const accentsTidy = (s) => {
+    var r=s.toLowerCase();
+    r = r.replace(new RegExp(/[àáâãäå]/g),"a");
+    r = r.replace(new RegExp(/ç/g),"c");
+    r = r.replace(new RegExp(/[èéêë]/g),"e");
+    r = r.replace(new RegExp(/[ìíîï]/g),"i");
+    r = r.replace(new RegExp(/[òóôõö]/g),"o");
+    r = r.replace(new RegExp(/[ùúûü]/g),"u");
+    return r;
+};
+
+const regExpTextCompare = (value) => new RegExp(`${accentsTidy(value)}`, 'ig');
+
+const searchTermArray = (arr = [], term = '') => {
+    let found = false;
+    arr.forEach((value) => {
+        if (regExpTextCompare(term).test(accentsTidy(value))) {
+            found = true;
+        }
+    })
+    return found;
+}
+
+const filterHightlights = (term, { hightlights }) => {
+    if (!term) return hightlights;
+    const hightlightsFiltered = hightlights.filter(({queries}) => searchTermArray(queries, term));
+
+    return hightlightsFiltered;
+};
 
 const filterSuggestions = (term, { suggestions }) => suggestions;
 
